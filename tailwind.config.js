8c2c00d5e6408e0687e00b0f85d2e68a6d2ca3cd
Mauro Ezequiel Paris <mauroparis96@gmail.com>
2020-02-27 15:12:09 -0300

    add landing page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily:{
+        courier: 'Courier Prime'
+      },
+      fontSize: {
+        '7xl': '7rem',
+        'xxl': '8rem',
+        '2xxl': '10rem',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
